d7cee8f0c6a528026915bb94f9891a74554030c7
jypark <jiyoonpark0207@gmail.com>
2021-08-30 22:34:15 +0900

    add face mesh

--- FaceMeshDetection/FaceMeshDetection.py
+++ FaceMeshDetection/FaceMeshDetection.py
@@ -2,9 +2,33 @@ import cv2
 import mediapipe as mp
 import time
 
+# cap = cv2.VideoCapture('../video/1.mp4')
 cap = cv2.VideoCapture(0)
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=4)
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = faceMesh.process(imgRGB)
+    if result.multi_face_landmarks:
+        for faceLms in result.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS,
+                                  landmark_drawing_spec=drawSpec)
+            for id, lm in enumerate(faceLms.landmark):
+                # print(lm)
+                ih, iw, ic = img.shape
+                x, y = int(lm.x * iw), int(lm.y * ih)
+                print(id, x, y)
+
+
+    cTime = time.time()
+    fps = 1/ (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f"FPS: {int(fps)}", (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0),3 )
     cv2.imshow("Image", img)
     cv2.waitKey(1)
